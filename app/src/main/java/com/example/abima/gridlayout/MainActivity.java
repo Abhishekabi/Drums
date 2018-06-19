0d47c0956ed290aa2d6a861c978afcb4a3c771a0
abhishek <abimano99@gmail.com>
2018-06-19 12:47:38 +0530

    Linear Layout to Grid Layout Modified...

--- app/src/main/java/com/example/abima/gridlayout/MainActivity.java
+++ app/src/main/java/com/example/abima/gridlayout/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.abima.gridlayout;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
